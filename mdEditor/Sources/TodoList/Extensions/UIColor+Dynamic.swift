847d068b86712a531c4c8b8203d6712ba2140269
D. P. <dp@MacBook-Pro.local>
2025-05-06 09:36:51 +0300

    add color scheme and extension

---
+++ mdEditor/Sources/TodoList/Extensions/UIColor+Dynamic.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Dynamic.swift
+//  mdEditor
+//
+//  Created by D. P. on 29.04.2025.
+//
+
+import Foundation
---
+++ mdEditor/Sources/TodoList/Extensions/UIColor+Hex.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Hex.swift
+//  mdEditor
+//
+//  Created by D. P. on 29.04.2025.
+//
+
+import Foundation
