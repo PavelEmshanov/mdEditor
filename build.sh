74df60d258fc5a227891dc483ab2f72abd7f968f
D. P. <dp@MacBook-Pro.local>
2025-05-11 20:13:25 +0300

    Обновление экшенов гит

---
+++ build.sh
@@ -0,0 +1,8 @@
+cd ./Project
+tuist fetch
+tuist generate
+xcodebuild clean -quiet
+xcodebuild build-for-testing\
+    -workspace 'MdEditor.xcworkspace' \
+    -scheme 'MdEditor' \
+    -destination 'platform=iOS Simulator,name=iPhone 14 Pro'
